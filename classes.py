dcebd9df470cda7b7661b8768aa3a992ffc304a5
ltreu <ltreu@student.ethz.ch>
2023-04-21 20:07:16 +0200

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,31 +1,41 @@
 import config
 
+class Block:
+
+    def __init__(self, name: str, rel_position: tuple) -> None:
+        
+        self.name = name                    # Minecraft block name
+        self.rel_position = rel_position    # Relative position to last jump block in parkour
+        self.abs_position = (0, 0, 0)       # Absolute coordinates in the minecraft world
+    
+    def set_abs_position(self, abs_coordinates: tuple):
+
+        self.abs_position = abs_coordinates
+
 class JumpType:
 
-    def __init__(self, name: str, structure_type: str, rel_start: tuple, rel_finish: tuple, blocks: list, difficulty: float, flow: float) -> None:
+    def __init__(self, name: str, structure_type: str, rel_start_block: Block, rel_finish_block: Block, blocks: list[Block], difficulty: float, flow: float) -> None:
         
         self.name = name
         self.structure_type = structure_type
-        self.rel_start = rel_start
-        self.rel_finish = rel_finish
+        self.rel_start_block = rel_start_block
+        self.rel_finish_block = rel_finish_block
         self.blocks = blocks
         
         self.difficulty = difficulty
         self.flow = flow
-
-        self.absolute_coordinates_of_start_block = (0, 0, 0)
     
     def set_absolut_coordinates(self, abs_tuple: tuple):
 
-        self.absolute_coordinates_of_start_block = abs_tuple
+        self.rel_start_block.set_abs_position(abs_tuple)
 
         for block in self.blocks:
 
-            abs_coordinates = self.compute_abs_coordinates(self.absolute_coordinates_of_start_block, block)
-            block.append(abs_coordinates)
+            abs_coordinates = self.compute_abs_coordinates(self.rel_start_block.abs_position, block)
+            block.set_abs_position(abs_coordinates)
 
         return
     
-    def compute_abs_coordinates(self, start_block, relative_block):
+    def compute_abs_coordinates(self, abs_start_block: tuple, relative_block: Block):
         pass
         
